064eaba1aad04e197f27a7381aa4f61c45d34e49
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-10 07:39:24 +0545

    Rename output div to ValutSectionShow for consistency and update references in JavaScript

--- js/Archive.js
+++ js/Archive.js
@@ -1,17 +1,17 @@
 /** @format */
 
 const showButton = document.getElementById("Others");
-const vaultDiv = document.getElementById("output");
+const vaultDiv = document.getElementById("ValutSectionShow");
 const draggableWindow = document.getElementById("draggableWindow");
 
-showButton.addEventListener("click", function () {
-  vaultDiv.style.display = "block"; // Make the div visible
+showButton.addEventListener("click", () => {
+  vaultDiv.style.display = "block";
 });
 
-vaultDiv.addEventListener("click", function () {
+vaultDiv.addEventListener("click", () => {
   window.location.href = "https://archive.adarashagaihre.com.np";
 });
 
-draggableWindow.addEventListener("click", function () {
+draggableWindow.addEventListener("click", () => {
   vaultDiv.style.display = "block";
 });
