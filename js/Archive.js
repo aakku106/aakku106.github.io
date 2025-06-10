6c1369c6411ec531005e07716affbf8579e1c2a5
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-10 08:53:58 +0545

    Refactor event handling for CrossBtnVault and ValutSectionShow in Archive.js

--- js/Archive.js
+++ js/Archive.js
@@ -1,16 +1,26 @@
 /** @format */
 
-const showButton = document.getElementById("Others");
-const vaultDiv = document.getElementById("ValutSectionShow");
-const CrossBtnVault = document.getElementById("CrossBtnVault");
+setTimeout(() => {
+  const showButton = document.getElementById("Others");
+  const vaultDiv = document.getElementById("ValutSectionShow");
+  const CrossBtnVault = document.getElementById("CrossBtnVault");
 
-showButton.addEventListener("click", () => {
-  vaultDiv.style.display = "block";
-});
+  CrossBtnVault.style.display = "none";
+  vaultDiv.style.display = "none";
 
-vaultDiv.addEventListener("click", () => {
-  window.location.href = "https://archive.adarashagaihre.com.np";
-});
-CrossBtnVault.addEventListener("click", () => {
-  window.location.href = "https://archive.adarashagaihre.com.np";
-});
+  showButton.addEventListener("click", () => {
+    vaultDiv.style.display = "block";
+    CrossBtnVault.style.display = "block";
+    CrossBtnVault.innerHTML = "X";
+  });
+
+  vaultDiv.addEventListener("click", () => {
+    window.location.href = "https://archive.adarashagaihre.com.np";
+  });
+
+  CrossBtnVault.addEventListener("click", () => {
+    CrossBtnVault.innerHTML = "";
+    vaultDiv.style.display = "none";
+    CrossBtnVault.style.display = "none";
+  });
+}, 2000);
